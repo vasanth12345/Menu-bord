15be1d0d2fa4eb2a076c55ea098abc60c8712a09
vasanth12345 <vasanthvp1611@gmail.com>
2021-06-16 20:10:14 +0530

    1c added files

---
+++ js/index.js
@@ -0,0 +1,447 @@
+const glass = [
+  {
+    id:1,
+    name: "blue",
+    prize:200,
+    decrib: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor ",
+    img: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.296vocMp085mZyi9Qd9bRAHaHa%26pid%3DApi&f=1",
+  },
+  {
+    id:2,
+    name: "black",
+    prize:170,
+    decrib: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor ",
+    img: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.Q45cRXfP-oLGcV3j-iUGDgAAAA%26pid%3DApi&f=1  ",
+  },
+  {
+    id:3,
+    name: "cars red",
+    prize:140,
+    decrib: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor ",
+    img: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.OdA-5oLHHu3hdh4yeWuIggHaJl%26pid%3DApi&f=1",
+  },
+  {
+    id:4,
+    name: "black & blue",
+    prize:130,
+    decrib: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor ",
+    img: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.0MEwbSzSmVy_s001mZGR-AHaHa%26pid%3DApi&f=1",
+  },
+  {
+    id:5,
+    name: "gradient sun glass",
+    prize:100,
+    decrib: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor ",
+    img: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.sWSbDjhw2P-KkBIiVappvwHaHa%26pid%3DApi&f=1",
+  },
+  {
+    id:6,
+    name: "violet",
+    prize:100,
+    decrib: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor ",
+    img: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.K6I4WyMp4wHq04LOtK6DLQHaHa%26pid%3DApi&f=1",
+  }
+  ];
+const watch = [
+  {
+    id:1,
+    name: "one",
+    prize:500,
+    decrib: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor ",
+    img: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.X-GVxI3d0XsrGT4A5n1HwwHaMn%26pid%3DApi&f=1",
+  },
+  {
+    id:2,
+    name: "two",
+    prize:550,
+    decrib: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor ",
+    img: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.wnXMNrFxKT1qjHdrdUaCrAHaKn%26pid%3DApi&f=1",
+  },
+  {
+    id:3,
+    name: "three",
+    prize:450,
+    decrib: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor ",
+    img: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.AdTEsNf5hPT15AyW_mGMUQHaKB%26pid%3DApi&f=1",
+  },
+  {
+    id:4,
+    name: "four",
+    prize:400,
+    decrib: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor ",
+    img: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.aOUgEFUh8v-Vgt6bF2BRFwDYEg%26pid%3DApi&f=1",
+  },
+  {
+    id:1,
+    name: "five",
+    prize:560,
+    decrib: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor ",
+    img: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.yqua7GHV5emVt1cX8fb1KwHaLJ%26pid%3DApi&f=1",
+  },
+  {
+    id:5,
+    name: "six",
+    prize:530,
+    decrib: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor ",
+    img: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.18meMJw2VLt198oG8Fd4VQHaLA%26pid%3DApi&f=1",
+  }
+  ];
+const shoes = [
+  {
+    id:1,
+    name: "orange",
+    prize:800,
+    decrib: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor ",
+    img: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.lYhkLJPO6mj8X0DIuC6dCwHaEt%26pid%3DApi&f=1",
+  },
+  {
+    id:2,
+    name: "blue",
+    prize:900,
+    decrib: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor ",
+    img: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.LkL1QWq0EtBYaleCxowNfQHaHa%26pid%3DApi&f=1",
+  },
+  {
+    id:3,
+    name: "pink",
+    prize:600,
+    decrib: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor ",
+    img: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.rpEaS9vPMwcRtNaJHVO59AHaHa%26pid%3DApi&f=1",
+  },
+  {
+    id:4,
+    name: "yellow",
+    prize:700,
+    decrib: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor ",
+    img: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.rpEaS9vPMwcRtNaJHVO59AHaHa%26pid%3DApi&f=1",
+  },
+  {
+    id:5,
+    name: "red",
+    prize:600,
+    decrib: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor ",
+    img: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.D5BtE7DHB76rE_ZyJnwa1wHaHa%26pid%3DApi&f=1",
+  },
+  {
+    id:6,
+    name: "white",
+    prize:660,
+    decrib: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor ",
+    img: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.CTpVBeL-wHpY8MOiqoDeAQHaHa%26pid%3DApi&f=1",
+  }
+  ];
+const caps =  [
+  {
+    id:1,
+    name: "dark blue",
+    prize:80,
+    decrib: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor ",
+    img: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.DYM2Fa_pOvxqX0a4gydD0wHaHa%26pid%3DApi&f=1",
+  },
+  {
+    id:2,
+    name: "black",
+    prize:70,
+    decrib: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor ",
+    img: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.O4uviv7ZJMCTIW8BAKrpnAHaGw%26pid%3DApi&f=1",
+  },
+  {
+    id:3,
+    name: "white",
+    prize:50,
+    decrib: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor ",
+    img: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.o0za3RL5xqDWXBoEU_j3uwHaHa%26pid%3DApi&f=1",
+  },
+  {
+    id:4,
+    name: "brown",
+    prize:60,
+    decrib: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor ",
+    img: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.UkPmWemN9DxFV30rgZVvYgHaHa%26pid%3DApi&f=1",
+  },
+  {
+    id:5,
+    name: "red",
+    prize:40,
+    decrib: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor ",
+    img: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.1z3P1nYuTH765eLiHg5KxAHaHa%26pid%3DApi&f=1",
+  },
+  {
+    id:6,
+    name: "green",
+    prize:90,
+    decrib: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor ",
+    img: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.tAr1hQ9fBh4liKrNLVIiMAHaHa%26pid%3DApi&f=1",
+  }
+  ];
+const shirt = [
+  {
+    id:1,
+    name: "red",
+    prize:450,
+    decrib: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor ",
+    img: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.8UGjzo9ArTWscwLmByld1AHaHa%26pid%3DApi&f=1",
+  },
+  {
+    id:2,
+    name: "white",
+    prize:450,
+    decrib: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor ",
+    img: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.Rmg4ntQJbbmRudtBpA6DpgHaHa%26pid%3DApi&f=1",
+  },
+  {
+    id:3,
+    name: "brown",
+    prize:400,
+    decrib: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor ",
+    img: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.iMGCIxgXO3Aq0rjq_hSogwHaHa%26pid%3DApi&f=1",
+  },
+  {
+    id:4,
+    name: "yellow",
+    prize:500,
+    decrib: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor ",
+    img: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.QQukdMx0s0c-2wgK1kNyvQHaHa%26pid%3DApi&f=1",
+  }
+  ];
+
+
+const box = document.querySelector('.product');
+const filtbtn = document.querySelectorAll('.btn');
+
+window.addEventListener('DOMContentLoaded', function(){
+ 
+ watchmenus(watch);
+  
+});
+
+
+filtbtn.forEach(function(btn){
+  
+btn.addEventListener('click', function(e){
+  
+const catagery = e.currentTarget.dataset.id;
+
+if (catagery === "watch") {
+ watchmenus(watch); 
+}
+else if(catagery === "glass"){
+ glassmenus(glass); 
+}
+else if(catagery === "Shirt"){
+ shirtmenus(shirt); 
+}
+else if(catagery === "caps"){
+ capsmenus(caps); 
+}
+else if(catagery === "shoes"){
+ shirtmenus(shoes); 
+}
+else{
+ watchmenus(watch); 
+}
+
+});
+
+});
+
+//all
+function allmenus(menuitem) {
+
+let menuFunction = menuitem.map(function (index) {
+  
+return `<div class="p-box">
+    
+    <div class="img">
+      <img class="p-img" src=${index.img} alt=${index.name} />
+    </div>
+    
+    <div class="second">
+      
+    <div class="p-name">${index.name}</div>
+     
+    <div class="p-prize">${index.prize}</div>
+    
+    </div>
+    
+    <div class="p-decrib">
+      ${index.decrib}
+    </div>
+    
+  </div> `;
+
+});
+
+menuFunction = menuFunction.join("");
+box.innerHTML = menuFunction;
+
+}
+
+//watch
+function watchmenus(menuitem) {
+
+let menuFunction = menuitem.map(function (index) {
+  
+return `<div class="p-box">
+    
+    <div class="img">
+      <img class="p-img" src=${index.img} alt=${index.name} />
+    </div>
+    
+    <div class="second">
+      
+    <div class="p-name">${index.name}</div>
+     
+    <div class="p-prize">${index.prize}</div>
+    
+    </div>
+    
+    <div class="p-decrib">
+      ${index.decrib}
+    </div>
+    
+  </div> `;
+
+});
+
+menuFunction = menuFunction.join("");
+box.innerHTML = menuFunction;
+
+}
+
+//glass
+function glassmenus(menuitem) {
+
+let menuFunction = menuitem.map(function (index) {
+  
+return `<div class="p-box">
+    
+    <div class="img">
+      <img class="p-img" src=${index.img} alt=${index.name} />
+    </div>
+    
+    <div class="second">
+      
+    <div class="p-name">${index.name}</div>
+     
+    <div class="p-prize">${index.prize}</div>
+    
+    </div>
+    
+    <div class="p-decrib">
+      ${index.decrib}
+    </div>
+    
+  </div> `;
+
+});
+
+menuFunction = menuFunction.join("");
+box.innerHTML = menuFunction;
+
+}
+
+//shirt
+function shirtmenus(menuitem) {
+
+let menuFunction = menuitem.map(function (index) {
+  
+return `<div class="p-box">
+    
+    <div class="img">
+      <img class="p-img" src=${index.img} alt=${index.name} />
+    </div>
+    
+    <div class="second">
+      
+    <div class="p-name">${index.name}</div>
+     
+    <div class="p-prize">${index.prize}</div>
+    
+    </div>
+    
+    <div class="p-decrib">
+      ${index.decrib}
+    </div>
+    
+  </div> `;
+
+});
+
+menuFunction = menuFunction.join("");
+box.innerHTML = menuFunction;
+
+}
+
+//caps
+function capsmenus(menuitem) {
+
+let menuFunction = menuitem.map(function (index) {
+  
+return `<div class="p-box">
+    
+    <div class="img">
+      <img class="p-img" src=${index.img} alt=${index.name} />
+    </div>
+    
+    <div class="second">
+      
+    <div class="p-name">${index.name}</div>
+     
+    <div class="p-prize">${index.prize}</div>
+    
+    </div>
+    
+    <div class="p-decrib">
+      ${index.decrib}
+    </div>
+    
+  </div> `;
+
+});
+
+menuFunction = menuFunction.join("");
+box.innerHTML = menuFunction;
+
+}
+
+//shoes
+function shoesmenus(menuitem) {
+
+let menuFunction = menuitem.map(function (index) {
+  
+return `<div class="p-box">
+    
+    <div class="img">
+      <img class="p-img" src=${index.img} alt=${index.name} />
+    </div>
+    
+    <div class="second">
+      
+    <div class="p-name">${index.name}</div>
+     
+    <div class="p-prize">${index.prize}</div>
+    
+    </div>
+    
+    <div class="p-decrib">
+      ${index.decrib}
+    </div>
+    
+  </div> `;
+
+});
+
+menuFunction = menuFunction.join("");
+box.innerHTML = menuFunction;
+
+}
+
+
+
+// card clicking 
+
+
+
+
+
